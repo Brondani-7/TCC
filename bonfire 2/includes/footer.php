242d494c359eac6db8a06995d3ad3e930a61b766
Brondani-7 <victorbrondanids@gmail.com>
2025-10-29 10:45:37 -0300

    Implement light mode toggle in footer
    Added light mode toggle functionality to footer.

--- bonfire 2/includes/footer.php
+++ bonfire 2/includes/footer.php
@@ -2,7 +2,20 @@
 <footer>
   <p>&copy; 2025 Bonfire. Todos os direitos reservados.</p>
 </footer>
+<script>
+  const toggle = document.getElementById('lightMode');
+  toggle.addEventListener('change', () => {
+    if (toggle.checked) {
+      document.documentElement.style.setProperty('--bg-color', '#E0FBFC');
+      document.documentElement.style.setProperty('--text-color', '#001B2E');
+    } else {
+      document.documentElement.style.setProperty('--bg-color', '#001B2E');
+      document.documentElement.style.setProperty('--text-color', '#E0FBFC');
+    }
+  });
+</script>
 </body>
 </html>
+
 
 
