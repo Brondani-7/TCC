350b086cd97478f2ad1acf33fe48a72468d15029
Brondani-7 <victorbrondanids@gmail.com>
2025-10-29 11:03:13 -0300

    Refactor footer.php by removing light mode script
    Removed unnecessary script for light mode toggle and cleaned up footer structure.

--- bonfire 2/includes/footer.php
+++ bonfire 2/includes/footer.php
@@ -2,15 +2,9 @@
 <footer>
   <p>&copy; 2025 Bonfire. Todos os direitos reservados.</p>
 </footer>
-<script>
-  const toggle = document.getElementById('lightMode');
-  toggle.addEventListener('change', () => {
-    document.body.classList.toggle('light-mode', toggle.checked);
-  });
-</script>
-
 </body>
 </html>
+
 
 
 
