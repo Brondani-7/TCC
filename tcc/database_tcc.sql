b0a2997d135d954e522b00f31e67e25916347cbb
Brondani-7 <victorbrondanids@gmail.com>
2025-11-25 09:35:40 -0300

    Add files via upload

--- tcc/database_tcc.sql
+++ tcc/database_tcc.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2025 at 04:25 AM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
+-- Tempo de geração: 25/11/2025 às 13:35
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,13 +18,13 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `database_tcc`
+-- Banco de dados: `database_tcc`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `fangames`
+-- Estrutura para tabela `fangames`
 --
 
 CREATE TABLE `fangames` (
@@ -41,10 +41,17 @@ CREATE TABLE `fangames` (
   `CreatedAt` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
+--
+-- Despejando dados para a tabela `fangames`
+--
+
+INSERT INTO `fangames` (`GameID`, `GameTitle`, `GameDescription`, `DeveloperID`, `Downloads`, `Rating`, `FileSize`, `Status`, `Genre`, `Franchise`, `CreatedAt`) VALUES
+(1, 'azimuth', 'jogo com grande historia e desenvolvimento envolvente', 3, 0, 0.00, NULL, 'Em Desenvolvimento', 'rpg', 'azmutalis', '2025-11-25 09:15:35');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `forum_categories`
+-- Estrutura para tabela `forum_categories`
 --
 
 CREATE TABLE `forum_categories` (
@@ -55,7 +62,7 @@ CREATE TABLE `forum_categories` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `forum_categories`
+-- Despejando dados para a tabela `forum_categories`
 --
 
 INSERT INTO `forum_categories` (`CategoryID`, `CategoryName`, `CategoryDescription`, `CreatedAt`) VALUES
@@ -80,7 +87,7 @@ INSERT INTO `forum_categories` (`CategoryID`, `CategoryName`, `CategoryDescripti
 -- --------------------------------------------------------
 
 --
--- Table structure for table `forum_likes`
+-- Estrutura para tabela `forum_likes`
 --
 
 CREATE TABLE `forum_likes` (
@@ -91,18 +98,21 @@ CREATE TABLE `forum_likes` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `forum_likes`
+-- Despejando dados para a tabela `forum_likes`
 --
 
 INSERT INTO `forum_likes` (`LikeID`, `PostID`, `CustomerID`, `CreatedAt`) VALUES
 (3, 21, 5, '2025-11-22 21:10:13'),
-(4, 22, 5, '2025-11-24 20:26:17'),
-(5, 23, 5, '2025-11-24 20:26:19');
+(5, 23, 5, '2025-11-24 20:26:19'),
+(7, 22, 3, '2025-11-25 09:25:33'),
+(8, 21, 3, '2025-11-25 09:26:30'),
+(9, 22, 5, '2025-11-25 09:29:10'),
+(10, 24, 5, '2025-11-25 09:29:12');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `forum_posts`
+-- Estrutura para tabela `forum_posts`
 --
 
 CREATE TABLE `forum_posts` (
@@ -119,7 +129,7 @@ CREATE TABLE `forum_posts` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `forum_posts`
+-- Despejando dados para a tabela `forum_posts`
 --
 
 INSERT INTO `forum_posts` (`PostID`, `TopicID`, `CustomerID`, `PostContent`, `CreatedAt`, `UpdatedAt`, `IsEdited`, `EditedBy`, `EditedAt`, `LikesCount`) VALUES
@@ -134,16 +144,16 @@ INSERT INTO `forum_posts` (`PostID`, `TopicID`, `CustomerID`, `PostContent`, `Cr
 (15, 7, 3, 'asasd', '2025-11-22 08:18:00', '2025-11-22 09:27:09', 1, 3, '2025-11-22 09:27:09', 0),
 (16, 7, 3, 'asda', '2025-11-22 08:31:09', '2025-11-22 08:31:09', 0, NULL, NULL, 0),
 (17, 7, 3, 'a', '2025-11-22 09:27:21', '2025-11-22 09:27:21', 0, NULL, NULL, 0),
-(21, 9, 3, 'a', '2025-11-22 11:18:42', '2025-11-22 11:18:42', 0, NULL, NULL, 1),
-(22, 9, 3, 'a', '2025-11-22 11:19:07', '2025-11-22 11:19:07', 0, NULL, NULL, 1),
+(21, 9, 3, 'a', '2025-11-22 11:18:42', '2025-11-22 11:18:42', 0, NULL, NULL, 2),
+(22, 9, 3, 'a', '2025-11-22 11:19:07', '2025-11-22 11:19:07', 0, NULL, NULL, 2),
 (23, 9, 3, 'op', '2025-11-22 20:14:15', '2025-11-22 20:14:15', 0, NULL, NULL, 1),
-(24, 9, 5, 'que supimpa!', '2025-11-22 21:10:09', '2025-11-22 21:10:09', 0, NULL, NULL, 0),
+(24, 9, 5, 'que supimpa!', '2025-11-22 21:10:09', '2025-11-22 21:10:09', 0, NULL, NULL, 1),
 (25, 11, 5, 'eu ainda nao acredito que isso aconteceu, alguem sabe se tem chance de voltar?', '2025-11-25 00:14:28', '2025-11-25 00:14:28', 0, NULL, NULL, 0);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `forum_topics`
+-- Estrutura para tabela `forum_topics`
 --
 
 CREATE TABLE `forum_topics` (
@@ -163,7 +173,7 @@ CREATE TABLE `forum_topics` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `forum_topics`
+-- Despejando dados para a tabela `forum_topics`
 --
 
 INSERT INTO `forum_topics` (`TopicID`, `CategoryID`, `TopicTitle`, `TopicDescription`, `CustomerID`, `CreatedAt`, `UpdatedAt`, `IsSticky`, `IsLocked`, `ViewCount`, `ReplyCount`, `LastPostBy`, `LastPostAt`) VALUES
@@ -173,15 +183,15 @@ INSERT INTO `forum_topics` (`TopicID`, `CategoryID`, `TopicTitle`, `TopicDescrip
 (4, 2, 'as', 'as', 4, '2025-11-18 11:45:53', '2025-11-18 11:45:59', 0, 0, 10, 2, 4, '2025-11-18 11:45:59'),
 (5, 18, 'estou em busca de jogos bons e baratos', 'alguem tem alguma recomendação?', 3, '2025-11-19 22:54:45', '2025-11-22 10:20:01', 0, 0, 9, 1, 3, '2025-11-19 22:54:45'),
 (7, 13, 'Brawl Stars', 'estou tendo dificuldade no rush de trofeus', 3, '2025-11-19 23:34:16', '2025-11-22 10:03:35', 0, 0, 25, 4, 3, '2025-11-22 09:27:21'),
-(8, 13, 'Brawl Stars', 'ganhei o novo brawler ziggy e queria dicas de como jogar', 3, '2025-11-22 10:04:36', '2025-11-22 10:04:36', 0, 0, 5, 1, 3, '2025-11-22 10:04:36'),
-(9, 13, 'Devil May Cry 5 - Bug do Braço gigante', 'descobri na batalha final de nero contra vergil, se voce tiver gerbera e buster arm voce pode usar a gerbera no momento exato de um dos ataques a sua devil breaker aparece bugada, com a buster arm aberta como se voçe estivesse usando ela', 3, '2025-11-22 10:12:00', '2025-11-22 21:10:09', 0, 0, 30, 5, 5, '2025-11-22 21:10:09'),
+(8, 13, 'Brawl Stars', 'ganhei o novo brawler ziggy e queria dicas de como jogar', 3, '2025-11-22 10:04:36', '2025-11-22 10:04:36', 0, 0, 6, 1, 3, '2025-11-22 10:04:36'),
+(9, 13, 'Devil May Cry 5 - Bug do Braço gigante', 'descobri na batalha final de nero contra vergil, se voce tiver gerbera e buster arm voce pode usar a gerbera no momento exato de um dos ataques a sua devil breaker aparece bugada, com a buster arm aberta como se voçe estivesse usando ela', 3, '2025-11-22 10:12:00', '2025-11-22 21:10:09', 0, 0, 51, 5, 5, '2025-11-22 21:10:09'),
 (10, 14, 'Red Dead Redemption 2', 'estou jogando a um tempo e queria saber como faço para pegar o crocodilo lendario', 3, '2025-11-22 10:15:05', '2025-11-22 10:15:05', 0, 0, 3, 1, 3, '2025-11-22 10:15:05'),
 (11, 17, 'need for speed', 'servidores de Need for Speed: Rivals desativados.', 5, '2025-11-25 00:14:28', '2025-11-25 00:14:28', 0, 0, 1, 1, 5, '2025-11-25 00:14:28');
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `foruns`
+-- Estrutura para tabela `foruns`
 --
 
 CREATE TABLE `foruns` (
@@ -194,7 +204,7 @@ CREATE TABLE `foruns` (
 -- --------------------------------------------------------
 
 --
--- Table structure for table `usuarios`
+-- Estrutura para tabela `usuarios`
 --
 
 CREATE TABLE `usuarios` (
@@ -212,35 +222,35 @@ CREATE TABLE `usuarios` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `usuarios`
+-- Despejando dados para a tabela `usuarios`
 --
 
 INSERT INTO `usuarios` (`CustomerID`, `CustomerGmail`, `CustomerName`, `CustomerPassword`, `CustomerHandle`, `CustomerBio`, `ProfileIcon`, `ProfilePhoto`, `ProfileBanner`, `CoverPhoto`, `CreatedAt`) VALUES
 (1, 'guilherme@gmail.com', 'masterplan', '$2y$10$sZ9KMLyh4JjucZ4zoBywiOyDqAD.llPJGPU5fW/frFecj0gGkLP62', 'leonjud2', 'eu sou o master plan', '🔥', 'uploads/profiles/profile_1_1763467767.jpg', NULL, 'uploads/cover_photos/cover_1_1762871705.gif', '2025-11-11 11:02:26'),
 (2, 'pobre@pobre.com', 'pobre', '$2y$10$oXz6UJ8P6nSQMgjPRPTPBuJqd3e4KJZHM.rd5tRvpc7AFOTspys/W', 'pobre', NULL, '🔥', NULL, NULL, NULL, '2025-11-11 11:05:55'),
-(3, 'ze@gmail.com', 'ze', '$2y$10$hIMrkaewCG9Vy2EBdVT61e18bsbGjtYrk0mOb/WlXTf4KMg/oW4P.', 'zebao', 'ze', '?', 'uploads/profiles/profile_3_1763853218.webp', NULL, NULL, '2025-11-18 10:26:07'),
+(3, 'ze@gmail.com', 'ze', '$2y$10$hIMrkaewCG9Vy2EBdVT61e18bsbGjtYrk0mOb/WlXTf4KMg/oW4P.', 'zebao', 'ze', '?', 'uploads/profiles/profile_3_1764072813.png', 'uploads/banners/banner_3_1764072817.png', NULL, '2025-11-18 10:26:07'),
 (4, 'pobre2@gmail.com', 'pobre da silva', '$2y$10$Me6PEdx73idNUPwCCfpA0egKn2hGMbkJw8cWkFzIb3mUULIA1xe.C', 'pobre2', 'ola', '?', 'uploads/profiles/profile_4_1763473646.png', NULL, NULL, '2025-11-18 10:30:40'),
-(5, 'fihdoze@gmail.com', 'fih do ze', '$2y$10$qgiIzccary8WzY0SZdwsIuKGkul2uGTjzxZwb0rP60YT37r3QVi3G', 'fihdoze', NULL, '?', 'uploads/profiles/profile_5_1763855930.png', 'uploads/banners/banner_5_1764040100.png', NULL, '2025-11-22 20:57:05');
+(5, 'fihdoze@gmail.com', 'fih do ze', '$2y$10$qgiIzccary8WzY0SZdwsIuKGkul2uGTjzxZwb0rP60YT37r3QVi3G', 'fihdoze', NULL, '?', 'uploads/profiles/profile_5_1764073738.png', 'uploads/banners/banner_5_1764040100.png', NULL, '2025-11-22 20:57:05');
 
 --
--- Indexes for dumped tables
+-- Índices para tabelas despejadas
 --
 
 --
--- Indexes for table `fangames`
+-- Índices de tabela `fangames`
 --
 ALTER TABLE `fangames`
   ADD PRIMARY KEY (`GameID`),
   ADD KEY `DeveloperID` (`DeveloperID`);
 
 --
--- Indexes for table `forum_categories`
+-- Índices de tabela `forum_categories`
 --
 ALTER TABLE `forum_categories`
   ADD PRIMARY KEY (`CategoryID`);
 
 --
--- Indexes for table `forum_likes`
+-- Índices de tabela `forum_likes`
 --
 ALTER TABLE `forum_likes`
   ADD PRIMARY KEY (`LikeID`),
@@ -248,7 +258,7 @@ ALTER TABLE `forum_likes`
   ADD KEY `CustomerID` (`CustomerID`);
 
 --
--- Indexes for table `forum_posts`
+-- Índices de tabela `forum_posts`
 --
 ALTER TABLE `forum_posts`
   ADD PRIMARY KEY (`PostID`),
@@ -257,7 +267,7 @@ ALTER TABLE `forum_posts`
   ADD KEY `EditedBy` (`EditedBy`);
 
 --
--- Indexes for table `forum_topics`
+-- Índices de tabela `forum_topics`
 --
 ALTER TABLE `forum_topics`
   ADD PRIMARY KEY (`TopicID`),
@@ -266,13 +276,13 @@ ALTER TABLE `forum_topics`
   ADD KEY `LastPostBy` (`LastPostBy`);
 
 --
--- Indexes for table `foruns`
+-- Índices de tabela `foruns`
 --
 ALTER TABLE `foruns`
   ADD PRIMARY KEY (`ForumID`);
 
 --
--- Indexes for table `usuarios`
+-- Índices de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
   ADD PRIMARY KEY (`CustomerID`),
@@ -280,70 +290,70 @@ ALTER TABLE `usuarios`
   ADD UNIQUE KEY `CustomerHandle` (`CustomerHandle`);
 
 --
--- AUTO_INCREMENT for dumped tables
+-- AUTO_INCREMENT para tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT for table `fangames`
+-- AUTO_INCREMENT de tabela `fangames`
 --
 ALTER TABLE `fangames`
-  MODIFY `GameID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
+  MODIFY `GameID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
--- AUTO_INCREMENT for table `forum_categories`
+-- AUTO_INCREMENT de tabela `forum_categories`
 --
 ALTER TABLE `forum_categories`
   MODIFY `CategoryID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
 
 --
--- AUTO_INCREMENT for table `forum_likes`
+-- AUTO_INCREMENT de tabela `forum_likes`
 --
 ALTER TABLE `forum_likes`
-  MODIFY `LikeID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `LikeID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT for table `forum_posts`
+-- AUTO_INCREMENT de tabela `forum_posts`
 --
 ALTER TABLE `forum_posts`
   MODIFY `PostID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
--- AUTO_INCREMENT for table `forum_topics`
+-- AUTO_INCREMENT de tabela `forum_topics`
 --
 ALTER TABLE `forum_topics`
   MODIFY `TopicID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
--- AUTO_INCREMENT for table `foruns`
+-- AUTO_INCREMENT de tabela `foruns`
 --
 ALTER TABLE `foruns`
   MODIFY `ForumID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT for table `usuarios`
+-- AUTO_INCREMENT de tabela `usuarios`
 --
 ALTER TABLE `usuarios`
   MODIFY `CustomerID` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- Constraints for dumped tables
+-- Restrições para tabelas despejadas
 --
 
 --
--- Constraints for table `fangames`
+-- Restrições para tabelas `fangames`
 --
 ALTER TABLE `fangames`
   ADD CONSTRAINT `fangames_ibfk_1` FOREIGN KEY (`DeveloperID`) REFERENCES `usuarios` (`CustomerID`);
 
 --
--- Constraints for table `forum_likes`
+-- Restrições para tabelas `forum_likes`
 --
 ALTER TABLE `forum_likes`
   ADD CONSTRAINT `forum_likes_ibfk_1` FOREIGN KEY (`PostID`) REFERENCES `forum_posts` (`PostID`),
   ADD CONSTRAINT `forum_likes_ibfk_2` FOREIGN KEY (`CustomerID`) REFERENCES `usuarios` (`CustomerID`);
 
 --
--- Constraints for table `forum_posts`
+-- Restrições para tabelas `forum_posts`
 --
 ALTER TABLE `forum_posts`
   ADD CONSTRAINT `forum_posts_ibfk_1` FOREIGN KEY (`TopicID`) REFERENCES `forum_topics` (`TopicID`),
@@ -351,7 +361,7 @@ ALTER TABLE `forum_posts`
   ADD CONSTRAINT `forum_posts_ibfk_3` FOREIGN KEY (`EditedBy`) REFERENCES `usuarios` (`CustomerID`);
 
 --
--- Constraints for table `forum_topics`
+-- Restrições para tabelas `forum_topics`
 --
 ALTER TABLE `forum_topics`
   ADD CONSTRAINT `forum_topics_ibfk_1` FOREIGN KEY (`CategoryID`) REFERENCES `forum_categories` (`CategoryID`),
